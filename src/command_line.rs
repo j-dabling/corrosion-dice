6384d76e251cb05a22507e713cbf7502b43610c9
Me <bryan.lurch@gmail.com>
2022-09-28 11:01:28 -0600

    CommandLine can handle and call one-word commands

--- src/command_line.rs
+++ src/command_line.rs
@@ -1,5 +1,6 @@
 type Callback = fn();
 pub mod command {
+	use std::fmt;
 	// Holds our command class attributes.
 	pub struct Command {
 		pub keyword: String, // The word that will trigger the callback
@@ -14,7 +15,8 @@ pub mod command {
 		// 	If no match is found, false is returned, used by the CommandLine object
 		// 	to keep looking.
 		pub fn verify(&self, args: Vec<String>) -> bool {
-			if args[0] == self.keyword {
+			println!("keyword for command: {self}, args being considered = {:?}", args);
+			if args[0].to_lowercase() == self.keyword {
 				(self.callback)();
 				return true
 			}
@@ -23,5 +25,46 @@ pub mod command {
 			}
 		}
 	}
+	impl fmt::Display for Command {
+		fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+			write!(f, "{}", self.keyword)
+		}
+	}
 }
 
+pub mod command_line {
+	use std::io::Write;
+	pub struct CommandLine {
+		pub command_list: Vec<crate::Command>,
+		pub prompt: String,
+	}
+	impl CommandLine {
+		pub fn get_input(&self) -> String {
+			let mut user_input = String::new();
+			print!("{}", self.prompt);
+			std::io::stdout().flush().expect("Couldn't flush the display");
+			std::io::stdin()
+				.read_line(&mut user_input)
+				.expect("Error reading line");
+			// user_input is now "<whatever_the_user_entered>\n"
+			//strip the trailing newline
+			user_input.pop();
+			// Now just "<whatever_the_user_entered>"
+			user_input
+		}
+		pub fn input_cycle(&self) {
+			let initial_user_input = self.get_input();
+			let intermediate_user_input = initial_user_input.split(' ');
+			let mut user_input = Vec::new();
+			for word in intermediate_user_input {
+				user_input.push(String::from(word));
+			}
+			// Now that we have a vector of strings, we can iterate through our commands and verify each in turn
+			for command in &self.command_list {
+				println!("{command}");
+				command.verify(user_input.clone());
+			}
+
+		}
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 // use std::io;
 mod command_line;
 use command_line::command::Command;
+use command_line::command_line::CommandLine;
 
 fn main() {
     let test_command = Command {
         keyword: String::from("bagels"),
         callback: test_function,
     };
+
     // let new_test_command = Command::new(new_test_command, String::from("bagels"), test_function);
-    let mut args : Vec<String> = Vec::new();
-    args.push(String::from("bagels"));
-    test_command.verify(args);
+    let mut test_command_list: Vec<Command> = Vec::new(); 
+    test_command_list.push(test_command);
+    let test_cli = CommandLine {
+        command_list: test_command_list,
+        prompt: String::from("enter your input: "),
+    };
+    test_cli.input_cycle();
+
 }
 
 fn test_function() {
