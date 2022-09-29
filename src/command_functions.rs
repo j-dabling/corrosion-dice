e6d817af0b0dcb30b8cb71f451dbe1f452e63cb0
Me <bryan.lurch@gmail.com>
2022-09-28 22:32:06 -0600

    make the value from roll20() 'roll' multiple times

--- src/command_functions.rs
+++ src/command_functions.rs
@@ -1,14 +1,25 @@
 pub mod command_functions {
 	// use std::io::Write;
-	// use std::thread::sleep;
-	// use std::time;
+	use std::{thread, time};
 	use std::io::Write;
 	use rand::Rng;
-	pub fn roll20() {
-		println!("rolling... [ ]\r");
+	pub fn roll20() -> i32 {
+		print!("rolling... [ ]\r");
 		std::io::stdout().flush().expect("couldn't flush the display");
-		// initially, we won't do the fancy stuff
+		thread::sleep(time::Duration::from_millis(100));
+		// This is the number of times that the 'die' will change before settling on the final result
+		let target = rand::thread_rng().gen_range(4..=20);
+		let mut i = 0;
+		while i < target {
+			let temp_result = rand::thread_rng().gen_range(1..=20);
+			print!("rolling... [ {temp_result} ]           \r");
+			std::io::stdout().flush().expect("couldn't flush the display");
+			thread::sleep(time::Duration::from_millis(i * 30));
+			i += 1;
+		}
 		let final_result = rand::thread_rng().gen_range(1..=20); // generate random integer between 1 and 20
-		println!("result: [{final_result}]");
+		println!("result: [ {final_result} ]                 ");
+		// the extra spaces are to make sure to overwrite the previous output
+		final_result // We might need to use this at some point.
 	}
 }
